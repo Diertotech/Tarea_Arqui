07709d02cd232ae7dc4df1569e7fcfa76546fcc3
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-10-13 21:28:13 -0300

    Add files via upload

--- Tarea1/frontend/src/App.js
+++ Tarea1/frontend/src/App.js
@@ -47,6 +47,30 @@ const DeleteResena = async (id) => {
     return myJson;
 };
 
+const UpdateResenaComentario = async (id,comentario) => {
+    const response = await fetch(`http://127.0.0.1:8080/resenaput2/${id}/${comentario}`, {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    const myJson = await response.json();
+    return myJson;
+};
+
+const UpdateResenaCalificacion = async (id,calificacion) => {
+    const response = await fetch(`http://127.0.0.1:8080/resenaput1/${id}/${calificacion}`, {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+    });
+    const myJson = await response.json();
+    return myJson;
+};
+var id_editar=-1;
 function App() {
     const [nombre, setName] = useState("");
     const [calificacion, setCalificacion] = useState(0);
@@ -56,6 +80,7 @@ function App() {
     const [busqueda, setBusqueda] = useState("");
     const [reseñas, setReseña] = useState([]);
     const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
 
     useEffect(() => {
         GetAllResena().then((data) => {
@@ -94,131 +119,208 @@ function App() {
         });
     }
 
+    function ManejarUpdate(id) {
+        id_editar=id
+        setModal2(!modal2)
 
-    return (
-        
-        <div className={styles.App}>
+    }
+    function Refresh(){
+        GetAllResena().then((data) => {
+            setReseña(data);
+            setModal2(!modal2)
+        });
+    }
 
-            <h1 className={styles.Title}>Peliculas</h1>
-            <Form>
-            <Label for="busqueda">Buscar Pelicula</Label>
-                <FormGroup>
 
-                    <Input className={styles.l1}
-                        type="text"
-                        name="busqueda"
-                        placeholder="Nombre de la pelicula"
-                        id="busqueda"
-                        value={busqueda}
-                        onChange={(e) => setBusqueda(e.target.value)}
-                    />
-            <Button className={styles.busqueda} color="primary" onClick={() => ManejarGetResenaByName()}>
-                BUSCAR
-            </Button>
-                </FormGroup>
-            </Form>
 
-            <Button color="primary" onClick={() => setModal(!modal)}>
-                AGREGAR Reseña
-            </Button>
-            <Table striped bordered>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nombre</th>
-                        <th>Calificacion</th>
-                        <th>Comentario</th>
-                        <th>Caratula</th>
-                        <th>Genero</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {reseñas.map((reseña, index) => (
-                        <tr key={index}>
-                            <th scope="row">{reseña.ID}  
 
-                            <Button color="primary" onClick={() => ManejarDeleteResena(reseña.ID)}>
-                            Eliminar Reseña
-                            </Button>
-                            
-                        </th>
-                            <td>{reseña.nombre}</td>
-                            <td>{reseña.calificacion}</td>
-                            <td>{reseña.comentario}</td>
-                            <td> <img src={reseña.caratula} alt="Imagen Caratula" width="500" height="600"/></td>
-                            <td>{reseña.genero}</td>
+
+        return (
+            
+            <div className={styles.App}>
+
+                <h1 className={styles.Title}>Peliculas</h1>
+                <Form>
+                <Label for="busqueda">Buscar Pelicula</Label>
+                    <FormGroup>
+
+                        <Input className={styles.l1}
+                            type="text"
+                            name="busqueda"
+                            placeholder="Nombre de la pelicula"
+                            id="busqueda"
+                            value={busqueda}
+                            onChange={(e) => setBusqueda(e.target.value)}
+                        />
+                <Button className={styles.busqueda} color="primary" onClick={() => ManejarGetResenaByName()}>
+                    BUSCAR
+                </Button>
+                    </FormGroup>
+                </Form>
+
+                <Button color="primary" onClick={() => setModal(!modal)}>
+                    AGREGAR Reseña
+                </Button>
+                <Table striped bordered>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Calificacion</th>
+                            <th>Comentario</th>
+                            <th>Caratula</th>
+                            <th>Genero</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {reseñas.map((reseña, index) => (
+                            <tr key={index}>
+                                <th scope="row">{reseña.ID}  
 
-            <Modal centered isOpen={modal}>
-                <ModalHeader>Agregar reseña</ModalHeader>
-                <ModalBody>
-                    <Form>
-                        <FormGroup style={{ marginBottom: "20px" }}>
-                            <Label for="name">Nombre</Label>
-                            <Input type="text" name="name" id="name" placeholder="Nombre Pelicula" value={nombre} onChange={(e) => setName(e.target.value)} />
-                        </FormGroup>
-                        <FormGroup style={{ marginBottom: "20px" }}>
-                            <Label for="calificacion">Calificacion</Label>
-                            <Input 
-                                type="number"
-                                name="calificacion"
-                                id="calificacion"
-                                placeholder="Calificacion Pelicula"
-                                min="0" 
-                                max="10"
-                                value={calificacion}
-                                onChange={(e) => setCalificacion(e.target.valueAsNumber)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="section">Comentario</Label>
-                            <Input
-                                type="text"
-                                name="section"
-                                id="section"
-                                placeholder="Comentario Pelicula"
-                                value={comentario}
-                                onChange={(e) => setSection(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="section">Caratula</Label>
-                            <Input
-                                type="text"
-                                name="caratula"
-                                id="caratula"
-                                placeholder="URL de la Foto Pelicula"
-                                value={caratula}
-                                onChange={(e) => setCaratula(e.target.value)}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="section">Genero</Label>
-                            <Input
-                                type="text"
-                                name="genero"
-                                id="genero"
-                                placeholder="Genero Pelicula"
-                                value={genero}
-                                onChange={(e) => setGenero(e.target.value)}
-                            />
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={() => ManejarNewResena()}>
-                        Agregar
-                    </Button>{" "}
-                    <Button color="secondary" onClick={() => setModal(!modal)}>
-                        Cancelar
-                    </Button>
-                </ModalFooter>
-            </Modal>
-        </div>
-    );
+                                <Button color="primary" onClick={() => ManejarDeleteResena(reseña.ID)}>
+                                Eliminar Reseña
+                                </Button>
+                                <Button color="primary" onClick={() => ManejarUpdate(reseña.ID)}>
+                                Editar Reseña
+                                </Button>
+                                
+                            </th>
+                                <td>{reseña.nombre}</td>
+                                <td>{reseña.calificacion}</td>
+                                <td>{reseña.comentario}</td>
+                                <td> <img src={reseña.caratula} alt="Imagen Caratula" width="500" height="600"/></td>
+                                <td>{reseña.genero}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+
+                <Modal centered isOpen={modal}>
+                    <ModalHeader>Agregar reseña</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup style={{ marginBottom: "20px" }}>
+                                <Label for="name">Nombre</Label>
+                                <Input type="text" name="name" id="name" placeholder="Nombre Pelicula" value={nombre} onChange={(e) => setName(e.target.value)} />
+                            </FormGroup>
+
+                            <FormGroup style={{ marginBottom: "20px" }}>
+                                <Label for="calificacion">Calificacion</Label>
+                                <Input 
+                                    type="number"
+                                    name="calificacion"
+                                    id="calificacion"
+                                    placeholder="Calificacion Pelicula"
+                                    min="0" 
+                                    max="10"
+                                    value={calificacion}
+                                    onChange={(e) => setCalificacion(e.target.valueAsNumber)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="section">Comentario</Label>
+                                <Input
+                                    type="text"
+                                    name="section"
+                                    id="section"
+                                    placeholder="Comentario Pelicula"
+                                    value={comentario}
+                                    onChange={(e) => setSection(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="section">Caratula</Label>
+                                <Input
+                                    type="text"
+                                    name="caratula"
+                                    id="caratula"
+                                    placeholder="URL de la Foto Pelicula"
+                                    value={caratula}
+                                    onChange={(e) => setCaratula(e.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="section">Genero</Label>
+                                <Input
+                                    type="text"
+                                    name="genero"
+                                    id="genero"
+                                    placeholder="Genero Pelicula"
+                                    value={genero}
+                                    onChange={(e) => setGenero(e.target.value)}
+                                />
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={() => ManejarNewResena()}>
+                            Agregar
+                        </Button>{" "}
+                        <Button color="secondary" onClick={() => setModal(!modal)}>
+                            Cancelar
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+
+
+
+
+
+
+
+                <Modal centered isOpen={modal2}>
+                    <ModalHeader>Editar Reseña</ModalHeader>
+                    <ModalBody>
+                        <Form>
+
+                            <FormGroup style={{ marginBottom: "20px" }}>
+                                <Label for="calificacion">Calificacion</Label>
+                                <Input 
+                                    type="number"
+                                    name="calificacion"
+                                    id="calificacion"
+                                    placeholder="Calificacion Pelicula"
+                                    min="0" 
+                                    max="10"
+                                    value={calificacion}
+                                    onChange={(e) => setCalificacion(e.target.valueAsNumber)}
+                                />
+                                <Button color="primary" onClick={() => UpdateResenaCalificacion(id_editar,calificacion)}>
+                                Editar Calificacion
+                                </Button>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="section">Comentario</Label>
+                                <Input
+                                    type="text"
+                                    name="section"
+                                    id="section"
+                                    placeholder="Comentario Pelicula"
+                                    value={comentario}
+                                    onChange={(e) => setSection(e.target.value)}
+                                />
+                                <Button color="primary" onClick={() => UpdateResenaComentario(id_editar,comentario)}>
+                                Editar Comentario
+                                </Button>
+                            </FormGroup>
+
+
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={() => Refresh()}>
+                            Aceptar
+                        </Button>{" "}
+                        <Button color="secondary" onClick={() => setModal2(!modal2)}>
+                            Cancelar
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+
+
+            </div>
+
+        );
 }
 
 export default App;
